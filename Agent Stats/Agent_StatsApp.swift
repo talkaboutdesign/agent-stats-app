6c79ca5e4bf88605d6e54d04b0b5d75ca7e2b977
Maksim Petriv <maksim.petriv@blueyonder.com>
2026-02-28 22:10:11 -0500

    Initial Commit

---
+++ Agent Stats/Agent_StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Agent_StatsApp.swift
+//  Agent Stats
+//
+//  Created by Max Petriv on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct Agent_StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
